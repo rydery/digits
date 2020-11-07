5e65556b260cf2d8cdb97d348d2378fc3734d9b1
rydery <70744064+rydery@users.noreply.github.com>
2020-11-06 22:25:59 -1000

    finished wod

--- app/imports/ui/pages/ListContactsAdmin.jsx
+++ app/imports/ui/pages/ListContactsAdmin.jsx
@@ -3,11 +3,11 @@ import { Meteor } from 'meteor/meteor';
 import { Container, Header, Loader, Card } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
-import Contact from '../components/Contact';
+import ContactAdmin from '../components/ContactAdmin';
 import { Contacts } from '../../api/contact/Contacts';
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
-class ListContacts extends React.Component {
+class ListContactsAdmin extends React.Component {
 
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
@@ -19,9 +19,9 @@ class ListContacts extends React.Component {
     return (
         /* error somewhere here */
         <Container>
-          <Header as="h2" textAlign="center" inverted>List Contacts</Header>
+          <Header as="h2" textAlign="center" inverted>List Contacts Admin</Header>
           <Card.Group>
-            {this.props.contacts.map((contact, index) => <Contact key={index} contact={contact}/>)}
+            {this.props.contacts.map((contact, index) => <ContactAdmin key={index} contact={contact}/>)}
           </Card.Group>
         </Container>
     );
@@ -29,7 +29,7 @@ class ListContacts extends React.Component {
 }
 
 /** Require an array of Stuff documents in the props. */
-ListContacts.propTypes = {
+ListContactsAdmin.propTypes = {
   contacts: PropTypes.array.isRequired,
   ready: PropTypes.bool.isRequired,
 };
@@ -37,9 +37,9 @@ ListContacts.propTypes = {
 /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
 export default withTracker(() => {
   // Get access to Stuff documents.
-  const subscription = Meteor.subscribe(Contacts.userPublicationName);
+  const subscription = Meteor.subscribe(Contacts.adminPublicationName);
   return {
     contacts: Contacts.collection.find({}).fetch(),
     ready: subscription.ready(),
   };
-})(ListContacts);
+})(ListContactsAdmin);
