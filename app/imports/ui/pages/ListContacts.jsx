272ccf9cee0266521f7a54642c0902a202dcfa8b
rydery <70744064+rydery@users.noreply.github.com>
2020-11-08 23:28:42 -1000

    wod attempt 2 DNF crashing

--- app/imports/ui/pages/ListContacts.jsx
+++ app/imports/ui/pages/ListContacts.jsx
@@ -1,45 +1,47 @@
 import React from 'react';
-import { Meteor } from 'meteor/meteor';
-import { Container, Header, Loader, Card } from 'semantic-ui-react';
-import { withTracker } from 'meteor/react-meteor-data';
+import { Card, Image, Feed } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
-import Contact from '../components/Contact';
-import { Contacts } from '../../api/contact/Contacts';
+import { withRouter, Link } from 'react-router-dom';
+import Note from './Note';
+import AddNote from './AddNote';
 
-/** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
-class ListContacts extends React.Component {
-
-  /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
+/** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
+class Contact extends React.Component {
   render() {
-    return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
-  }
-
-  /** Render the page once subscriptions have been received. */
-  renderPage() {
     return (
-        /* error somewhere here */
-        <Container>
-          <Header as="h2" textAlign="center" inverted>List Contacts</Header>
-          <Card.Group>
-            {this.props.contacts.map((contact, index) => <Contact key={index} contact={contact}/>)}
-          </Card.Group>
-        </Container>
+        <Card centered>
+          <Card.Content>
+            <Image
+                floated='right'
+                size='mini'
+                src={this.props.contact.image}
+            />
+            <Card.Header>{this.props.contact.firstName} {this.props.contact.lastName}</Card.Header>
+            <Card.Meta>{this.props.contact.address}</Card.Meta>
+            <Card.Description>
+              {this.props.contact.description}
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <Link to={`/edit/${this.props.contact._id}`}>Edit</Link>
+          </Card.Content>
+          <Card.Content extra>
+            <Feed>
+              {this.props.notes.map((note, index) => <Note key={index} note={note}/>)}
+            </Feed>
+          </Card.Content>
+          <Card.Content extra>
+            <AddNote owner={this.props.contact.owner} contactId={this.props.contact._id}/>
+          </Card.Content>
+        </Card>
     );
   }
 }
-
-/** Require an array of Stuff documents in the props. */
-ListContacts.propTypes = {
-  contacts: PropTypes.array.isRequired,
-  ready: PropTypes.bool.isRequired,
+/** Require a document to be passed to this component. */
+Contact.propTypes = {
+  contact: PropTypes.object.isRequired,
+  notes: PropTypes.array.isRequired,
 };
 
-/** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
-export default withTracker(() => {
-  // Get access to Stuff documents.
-  const subscription = Meteor.subscribe(Contacts.userPublicationName);
-  return {
-    contacts: Contacts.collection.find({}).fetch(),
-    ready: subscription.ready(),
-  };
-})(ListContacts);
+/** Wrap this component in withRouter since we use the <Link> React Router element. */
+export default withRouter(Contact);
